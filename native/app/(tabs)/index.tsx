1ccece7e44ee0739f66131e15321fe0c1765238a
lobor <lionel.bertrand@paymium.com>
2023-07-27 13:37:48 +0200

    first init

---
+++ native/app/(tabs)/index.tsx
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '@/components/EditScreenInfo';
+import { Text, View } from '@/components/Themed';
+import { FlexDemo } from '@crossed/demo';
+import {styled} from "nativewind" 
+
+const Toto = styled(View)
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      <FlexDemo />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
